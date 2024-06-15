f5037bf0937939bb948aa98e665c9f55bdbb7276
K3vR0ld4n <kroldan@espol.edu.ec>
2024-06-15 18:07:48 -0500

    shopping cart section added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { IndexComponent } from './pages/index/index.component';
-
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 
 export const routes: Routes = [
     { path: '', component: IndexComponent },
+    {path:'shoppingCart',component:ShoppingCartComponent},
 
     { path: '**', redirectTo: '' },
 ];

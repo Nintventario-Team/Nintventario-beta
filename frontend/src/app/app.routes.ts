4a17e44226dc080d03ffc71221ed49a056ece6c6
K3vR0ld4n <kroldan@espol.edu.ec>
2024-06-15 20:34:21 -0500

    blog section added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,8 +3,11 @@ import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
 import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 import {ProductSectionComponent} from './pages/product-section/product-section.component'
+import {BlogComponent } from './pages/blog/blog.component'
+
 export const routes: Routes = [
     { path: '', component: IndexComponent },
+    {path: 'blog', component: BlogComponent},
     { path: 'videojuegos', component: ProductSectionComponent, data: { section: 'videojuegos' } },
     { path: 'funkopop', component: ProductSectionComponent, data: { section: 'funkopop' } },
     { path: 'consolas', component: ProductSectionComponent, data: { section: 'consolas' } },

7564208f9421357191bbff46d65a13aef340128b
K3vR0ld4n <kroldan@espol.edu.ec>
2024-06-16 14:45:09 -0500

    merge

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,13 +2,22 @@
 import {  Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { AppComponent } from './app.component';
 import { IndexComponent } from './pages/index/index.component';
+import {ProductSectionComponent} from './pages/product-section/product-section.component'
+import {BlogComponent } from './pages/blog/blog.component'
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 
 export const routes: Routes = [
     { path: '', component: IndexComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: '**', redirectTo: '/' },
+    { path: 'blog', component: BlogComponent},
+    { path: 'videojuegos', component: ProductSectionComponent, data: { section: 'videojuegos' } },
+    { path: 'funkopop', component: ProductSectionComponent, data: { section: 'funkopop' } },
+    { path: 'consolas', component: ProductSectionComponent, data: { section: 'consolas' } },
+    { path: 'coleccionables', component: ProductSectionComponent, data: { section: 'coleccionables' } },
+    { path:'shoppingCart',component:ShoppingCartComponent},
+
+    { path: '**', redirectTo: '' },
   ];
 
